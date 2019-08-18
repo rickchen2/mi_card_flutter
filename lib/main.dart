7754961c5d8a19e5aa6c0fa77707981794256699
Hongyu Chen <hongyu.chen@traderev.com>
2019-08-18 10:23:48 -0400

    update yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/default_profile.jpg'),
+              ),
+              Text(
+                'Rick',
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
